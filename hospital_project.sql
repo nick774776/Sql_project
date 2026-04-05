a16990345c38a8ad79186e04262a8d30064a40a4
Nikhil Kumar <knikhil.niper2025@gmail.com>
2026-04-05 12:20:15 +0530

    Add files via upload

---
+++ hospital_project.sql
@@ -0,0 +1,14 @@
+create table Patient_data_1 
+(
+Hospital_Name varchar(100),
+Location varchar(100),		
+Department varchar(100),
+Doctors_Count int,
+Patients_Count int,
+Admission_Date date,
+Discharge_Date date,
+Medical_Expenses int
+);
+
+select * from Patient_data_1;
+
